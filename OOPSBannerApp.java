f0e00af3086f55bc794d8cad79ea1710db536578
ak6014-srmist <ak6014@srmist.edu.in>
2026-02-16 09:21:39 +0530

    Add OOPSBannerApp to print 'OOPS'

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * UC1 - Print literal OOPS
+ * @author Ashwin
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
