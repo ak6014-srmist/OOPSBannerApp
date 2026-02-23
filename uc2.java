c794f516b04c7ba4d384ca8731ff0267f32a6e7a
ak6014-srmist <ak6014@srmist.edu.in>
2026-02-23 09:05:58 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,37 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ *
+ * Displays the word "OOPS" in banner format
+ * using ASCII characters.
+ *
+ * @author Developer
+ * @version 2
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+
+        // Line 2
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
+
+        // Line 3
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
+
+        // Line 4
+        System.out.println(" **   ** " + " **   ** " + " ******  " + "  *****  ");
+
+        // Line 5
+        System.out.println(" **   ** " + " **   ** " + " **      " + "      ** ");
+
+        // Line 6
+        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+
+        // Line 7
+        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
+
+    }
+}
