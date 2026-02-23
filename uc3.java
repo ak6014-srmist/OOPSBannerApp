2dfe6736a8dafd92c8904744c59c97f372ac2f20
ak6014-srmist <ak6014@srmist.edu.in>
2026-02-23 09:08:18 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,72 @@
+/**/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ *
+ * Displays the word "OOPS" in banner format
+ * using String.join() for better readability.
+ *
+ * @author Developer
+ * @version 3.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ******",
+                " ***** "
+        ));
+
+        // Line 2
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**   **"
+        ));
+
+        // Line 3
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**     "
+        ));
+
+        // Line 4
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "****** ",
+                " ***** "
+        ));
+
+        // Line 5
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**     ",
+                "     **"
+        ));
+
+        // Line 6
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**     ",
+                "**   **"
+        ));
+
+        // Line 7
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                "**     ",
+                " ***** "
+        ));
+
+    }
+}
